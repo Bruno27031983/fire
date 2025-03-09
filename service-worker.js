7067361ea4fdcaac58aa958bf8cfe726ced87cfc
Bruno27031983 <brunteles123456789@gmail.com>
2025-03-09 19:58:17 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,49 @@
-const CACHE_NAME = 'calculator-cache-v1';
+// Definovanie názvu cache a zoznamu URL, ktoré chceme cache-ovať
+const CACHE_NAME = 'brunos-calculator-cache-v1';
 const urlsToCache = [
-  '/fire/',
-  '/fire/index.html',
-  '/fire/manifest.json',
-  '/fire/service-worker.js',
-  'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js',
-  'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js',
-  'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+  './',               // Hlavná stránka
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+  // Pridajte ďalšie súbory, ktoré chcete cache-ovať (CSS, JS, obrázky, ...)
 ];
 
+// Inštalácia Service Worker a cacheovanie zdrojov
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('Otvorená cache a ukladám súbory');
+        console.log('Cache otvorená');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
+// Obsluha požiadaviek - vrátenie odpovede z cache, ak je k dispozícii
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request)
-          .then((response) => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          })
-          .catch(() => {
-            return caches.match('/fire/index.html');
-          });
+        // Ak je odpoveď v cache, vrátime ju, inak získame zo siete
+        return response || fetch(event.request);
       })
+  );
+});
+
+// Aktivácia Service Worker a odstraňovanie starých cache
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            console.log('Odstraňujem starú cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
